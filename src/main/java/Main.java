b99b20f932105d798bcddba201fed7fee5dddf2c
erick2507 <169068468+erick2507@users.noreply.github.com>
2024-09-02 12:23:07 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+int contqdor = 1;
+String nome = "Erick";
+    
+while(contador < 11) {
+  System.out.println("bom dia");
+  contador = contador + 1;
+  }
+
+  
+  }
+System.out.println();
+
+  
+}
